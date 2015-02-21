505d545847854cb4061a3832e2570e2b5aaefacd
vkfont <vkfont@gmail.com>
2015-02-21 20:44:56 +0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+#!/usr/bin/env node
+
 'use strict';
 
-module.exports = {
-    Service: require('./service'),
-    common: require('./common')
-};
+var core = require('paukan-core');
+var async = require('async');
+var Scenario = require('./lib/scenario');
+
+var config = core.common.serviceConfig(require('./config.json'), require('./package.json'));
+
+// var service, http;
+async.series([
+    function (next) {
+        service = new core.Service(config, next);
+    },
+    function (next) {
+        scene = new Scenario();
+    }
+], function (err) {
+    if(err) { throw err; }
+    console.log('Web service started on %s port', config.listen);
+});
